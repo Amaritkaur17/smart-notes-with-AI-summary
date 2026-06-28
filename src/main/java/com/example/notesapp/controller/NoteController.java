ef1ea6040e4944a5711d428b783af14b4e1d68fa
Amaritkaur17 <amarit17kaur@gmail.com>
2026-06-28 23:24:16 +0530

    Implemented NoteController

--- src/main/java/com/example/notesapp/controller/NoteController.java
+++ src/main/java/com/example/notesapp/controller/NoteController.java
@@ -1,7 +1,36 @@
 package com.example.notesapp.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.notesapp.entity.Note;
+import com.example.notesapp.service.NoteService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/notes")
 public class NoteController {
+
+    private final NoteService noteService;
+
+    public NoteController(NoteService noteService){
+        this.noteService = noteService;
+    }
+
+    @PostMapping
+    public Note createNote(@RequestBody Note note){
+        return noteService.createNote(note);
+    }
+    @GetMapping
+    public List<Note> getAllNotes(){
+        return noteService.getAllNotes();
+    }
+    @GetMapping("/{id}")
+    public Optional<Note> getNoteById(@PathVariable Long id){
+        return noteService.getNoteById(id);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteNote(@PathVariable Long id){
+        noteService.deleteNote(id);
+    }
 }
