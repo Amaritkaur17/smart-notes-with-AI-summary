083f9ab92195cd7827611fccfcb069308bf7f892
Amaritkaur17 <amarit17kaur@gmail.com>
2026-06-28 19:05:00 +0530

    Implement NoteService CRUD operations

--- src/main/java/com/example/notesapp/repository/NoteRepository.java
+++ src/main/java/com/example/notesapp/repository/NoteRepository.java
@@ -3,5 +3,19 @@ package com.example.notesapp.repository;
 import com.example.notesapp.entity.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/* Why extend JpaRepository<Note, Long>?
+        Note → the entity this repository manages.
+        Long → the type of the entity's primary key (id).
+
+        By extending JpaRepository, you automatically get methods such as:
+        save(note);
+        findById(id);
+        findAll();
+        deleteById(id);
+        existsById(id);
+        count();
+*/
+
+
 public interface NoteRepository extends JpaRepository<Note,Long> {
 }
--- src/main/java/com/example/notesapp/service/NoteService.java
+++ src/main/java/com/example/notesapp/service/NoteService.java
@@ -1,7 +1,34 @@
 package com.example.notesapp.service;
 
+import com.example.notesapp.entity.Note;
+import com.example.notesapp.repository.NoteRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class NoteService {
+
+    private final NoteRepository noteRepository;
+
+    public NoteService(NoteRepository noteRepository){
+        this.noteRepository = noteRepository;
+    }
+
+    public Note createNote(Note note){
+        return noteRepository.save(note);
+    }
+
+    public List<Note> getAllNotes(){
+        return noteRepository.findAll();
+    }
+
+    public Optional<Note> getNoteById(Long id){
+        return noteRepository.findById(id);
+    }
+
+    public void deleteNote(Long id){
+        noteRepository.deleteById(id);
+    }
 }
