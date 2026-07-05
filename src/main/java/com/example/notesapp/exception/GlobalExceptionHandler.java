1016dc2897079e0be7451f8688fc39ce43b25ae1
Amaritkaur17 <amarit17kaur@gmail.com>
2026-07-05 15:39:02 +0530

    Return structured JSON error response

--- src/main/java/com/example/notesapp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/notesapp/exception/GlobalExceptionHandler.java
@@ -1,15 +1,35 @@
 package com.example.notesapp.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.time.LocalDateTime;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(ResourceNotFoundException.class)
+    /*@ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> handleResouceNotFoundException(ResourceNotFoundException ex){
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }*/
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResouceNotFoundException(ResourceNotFoundException ex,
+                                                                        HttpServletRequest request)
+    {
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+
+        return new ResponseEntity<>(errorResponse,HttpStatus.NOT_FOUND);
+
+
     }
 }
