ca20356d4022af514ed584ffaa0c2bafa28c3c85
Amaritkaur17 <amarit17kaur@gmail.com>
2026-07-20 09:40:43 +0530

    add logging for note service operations

--- src/main/java/com/example/notesapp/service/NoteService.java
+++ src/main/java/com/example/notesapp/service/NoteService.java
@@ -3,25 +3,32 @@ package com.example.notesapp.service;
 import com.example.notesapp.entity.Note;
 import com.example.notesapp.exception.ResourceNotFoundException;
 import com.example.notesapp.repository.NoteRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+
 @Service
 public class NoteService {
 
     private final NoteRepository noteRepository;
+    private static final Logger logger =LoggerFactory.getLogger(NoteService.class);
 
     public NoteService(NoteRepository noteRepository){
         this.noteRepository = noteRepository;
     }
 
     public Note createNote(Note note){
+        logger.info("Creating a new not with title : {}",note.getTitle());
         note.setCreatedAt(LocalDateTime.now());
         note.setUpdatedAT(LocalDateTime.now());
-        return noteRepository.save(note);
+        Note savedNote =  noteRepository.save(note) ;
+        logger.info("Note created sucessfully with id : {}",savedNote.getId());
+        return savedNote;
     }
 
     public List<Note> getAllNotes(){
@@ -29,12 +36,14 @@ public class NoteService {
     }
 
     public Note getNoteById(Long id){
-     return noteRepository.findById(id)
+        logger.warn("Fetching note with id: {}", id);
+        return noteRepository.findById(id)
                     .orElseThrow(() ->
                             new ResourceNotFoundException("Note not found with ID : " + id));
         }
 
     public void deleteNote(Long id){
+        logger.info("Deleted the note with id : {}", id) ;
         noteRepository.deleteById(id);
     }
     public Note updateNote(Long id,Note updatedNote){
@@ -44,8 +53,10 @@ public class NoteService {
             note.setTitle(updatedNote.getTitle());
             note.setContent(updatedNote.getContent());
             note.setUpdatedAT(LocalDateTime.now());
+            logger.info("Updating note with id : {}", note.getId());
             return noteRepository.save(note);
         }
+
         //return null;
         throw new ResourceNotFoundException("Note not found with ID :"+id);
     }
